e90d1376a0eb040a4de0e0a8b553338c5326fdcf
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-02-25 09:23:09 +0300

    set up the urlpatterns for my views as well as set up a specific way to view individual photos

--- snaps/views.py
+++ snaps/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+def gallery(request):
+    return render(request,'snaps/gallery.html')
+
+def viewSnap(request, pk):
+    return render(request,'snaps/photo.html')
+
+def addSnap(request):
+    return render(request,'snaps/add.html')
+
 # Create your views here.
