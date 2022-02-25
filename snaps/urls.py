407d922c65f80c81cf6efd95f728a7ee0f57a19c
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-02-25 09:32:42 +0300

    made sure all my views worked

--- snaps/urls.py
+++ snaps/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.gallery, name='gallery',)
-    path('photo/<str:pk>',views.viewSnap, name='photo',)
-    path('add/',views.addSnap, name='add', )
+    path('',views.gallery, name='gallery'),
+    path('snap/<str:pk>/',views.viewSnap, name='photo',),
+    path('add/',views.addSnap, name='add' ),
 ]
