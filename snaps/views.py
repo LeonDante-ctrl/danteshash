0d15fc495a32420df37e54565c8c4a9ad2562cf5
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-02-25 11:25:23 +0300

    added in the linking to both pages and on my snap page set up a button to return me to the home page

--- snaps/views.py
+++ snaps/views.py
@@ -5,7 +5,7 @@ def gallery(request):
     return render(request,'snaps/gallery.html')
 
 def viewSnap(request, pk):
-    return render(request,'snaps/photo.html')
+    return render(request,'snaps/snap.html')
 
 def addSnap(request):
     return render(request,'snaps/add.html')
