f748e0cb298beb78d9182ce6acff4364897346f9
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-02-25 12:42:15 +0300

    set up the db so that when an image is uploaded, it goes to a particular space

--- snaps/models.py
+++ snaps/models.py
@@ -1,3 +1,19 @@
+from email.mime import image
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Snaps(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True , blank=True)
+    image = models.ImageField(null=False, blank=False)
+    name = models.TextField()
+    
+    def __str__(self):
+        return self.description  
