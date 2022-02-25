9c4b0f13bbfdea284ee6c1d6cc0cfd6fc305d4f9
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-02-25 17:11:03 +0300

    fixed ability to only show images based on category

--- snaps/views.py
+++ snaps/views.py
@@ -1,11 +1,17 @@
+from email.mime import image
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category, Snaps
 
 
 def gallery(request):
-    categories = Category.objects.all()
-    snaps = Snaps.objects.all
+    category = request.GET.get('category')
+    if category == None:
+     snaps = Snaps.objects.all()
+    else:
+        snaps = Snaps.objects.filter(category__name=category)
+        
+    categories = Category.objects.all()   
     context = {'categories' : categories, 'snaps':snaps}
     return render(request,'snaps/gallery.html', context)
 
@@ -14,6 +20,30 @@ def viewSnap(request, pk):
     return render(request,'snaps/snap.html', {'snap':snap} )
 
 def addSnap(request):
-    return render(request,'snaps/add.html')
+    categories = Category.objects.all()
+    
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('image')
+        
+        if data['category'] != 'none':
+                category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create( name=data['category_new'])
+        else:
+            category = None
+            
+        snap = Snaps.objects.create(
+                category=category,
+                name=data['description'],
+                image=image,
+            )
+        return redirect('gallery')
+       # print('data:', data)
+        # print('image:', image)
+        
+    
+    context = {'categories' : categories, }
+    return render(request,'snaps/add.html', context)
 
 # Create your views here.
